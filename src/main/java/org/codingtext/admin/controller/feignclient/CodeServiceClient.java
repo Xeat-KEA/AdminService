f0605f65f0497bbf2247717202026117c3bf516c
parkjaehak <atom8426@gmail.com>
2024-11-03 20:48:43 +0900

    add: 특정 문제를 삭제하기 위한 feign client 설정

--- src/main/java/org/codingtext/admin/controller/feignclient/CodeServiceClient.java
+++ src/main/java/org/codingtext/admin/controller/feignclient/CodeServiceClient.java
@@ -1,21 +1,20 @@
 package org.codingtext.admin.controller.feignclient;
 
-import org.codingtext.admin.dto.problem.ProblemListResponse;
-import org.codingtext.admin.dto.problem.ProblemRequest;
-import org.codingtext.admin.dto.problem.ProblemResponse;
+import org.codingtext.admin.dto.problem.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient(name = "code-bank-service", url = "http://172.16.211.69:8080")
 public interface CodeServiceClient {
-    @GetMapping("/codes")
+    @GetMapping("/code")
     List<ProblemListResponse> getProblemList();
 
-    @PostMapping("/codes")
+    @PostMapping("/code")
     ResponseEntity<ProblemResponse> createProblem(@RequestBody ProblemRequest problemRequest);
+
+    @DeleteMapping("/code/{codeId}")
+    ResponseEntity<?> deleteProblem(@PathVariable("codeId") long codeId);
 }
