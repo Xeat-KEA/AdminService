7f66d0881357def78b6fb7cd52d324abb534f187
parkjaehak <atom8426@gmail.com>
2024-11-03 20:48:43 +0900

    feat: 새로운 문제 생성 feign client

---
+++ src/main/java/org/codingtext/admin/dto/problem/ProblemResponse.java
@@ -0,0 +1,19 @@
+package org.codingtext.admin.dto.problem;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProblemResponse {
+    private long id;
+    private String title;
+    private String difficulty;
+    private String algorithm;
+    private String content;
+    private List<String> input;
+    private String output;
+}
