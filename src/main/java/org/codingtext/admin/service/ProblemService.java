24966c71d98902590a54f83aeafbc7fb282b452b
parkjaehak <atom8426@gmail.com>
2024-11-05 17:33:15 +0900

    add: error response 추가

--- src/main/java/org/codingtext/admin/service/ProblemService.java
+++ src/main/java/org/codingtext/admin/service/ProblemService.java
@@ -3,6 +3,9 @@ package org.codingtext.admin.service;
 import lombok.RequiredArgsConstructor;
 import org.codingtext.admin.controller.feignclient.CodeServiceClient;
 import org.codingtext.admin.dto.problem.*;
+import org.codingtext.admin.error.exception.ProblemCreationFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +21,15 @@ public class ProblemService {
     }
 
     public ProblemResponse createProblem(ProblemRequest problemRequest) {
-        return codeServiceClient.createProblem(problemRequest);
+        ResponseEntity<ProblemResponse> response = codeServiceClient.createProblem(problemRequest);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        } else if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
+            throw new ProblemCreationFailedException("잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+        } else if (response.getStatusCode() == HttpStatus.CONFLICT) {
+            throw new ProblemCreationFailedException("문제가 이미 존재합니다.", HttpStatus.CONFLICT);
+        } else {
+            throw new ProblemCreationFailedException("문제 생성에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
