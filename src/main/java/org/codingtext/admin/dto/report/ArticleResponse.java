72ab44697f9ccc7f3e761ab3e93e624136d1646e
parkjaehak <atom8426@gmail.com>
2024-11-09 15:54:58 +0900

    feat: 댓글 신고내역 조회

--- src/main/java/org/codingtext/admin/dto/report/ArticleResponse.java
+++ src/main/java/org/codingtext/admin/dto/report/ArticleResponse.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ArticleResponse {
     Long articleId; //해당 게시글 표시
-    String title; //신고 게시글 제목
+    String articleTitle; //신고 게시글 제목
     String name; //신고자
     ReportType reportType; //신고사유
     LocalDate reportDate; //신고일
