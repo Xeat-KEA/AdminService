5592cc630c1b5f0cc00ad92f78b5a0b1a5d64720
parkjaehak <atom8426@gmail.com>
2024-11-21 09:33:12 +0900

    add: eureka client 설정

--- src/main/java/org/codingtext/admin/AdminApplication.java
+++ src/main/java/org/codingtext/admin/AdminApplication.java
@@ -2,10 +2,14 @@ package org.codingtext.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
+@EnableJpaAuditing
 public class AdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
