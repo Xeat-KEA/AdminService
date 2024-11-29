7242110f7b7bb4cd18aebb2e6dcaa4d69864db8d
parkjaehak <atom8426@gmail.com>
2024-11-29 23:41:18 +0900

    add: 에러시 디버깅을 위한 로그 추가

--- src/main/java/org/codingtext/admin/error/GlobalExceptionHandler.java
+++ src/main/java/org/codingtext/admin/error/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.codingtext.admin.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.codingtext.admin.error.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,22 +8,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PermissionDeniedException.class)
     public ResponseEntity<ErrorResponse> handleAdminDeletionException(PermissionDeniedException ex) {
+        log.error("PermissionDeniedException : {}", ex.getMessage(), ex);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(AdminNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(AdminNotFoundException ex) {
+        log.error("AdminNotFoundException : {}", ex.getMessage(), ex);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
     public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidPasswordException ex) {
+        log.error("InvalidPasswordException : {}", ex.getMessage(), ex);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
@@ -30,18 +35,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AnnounceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleAnnounceNotFoundExceptionException(AnnounceNotFoundException ex) {
+        log.error("AnnounceNotFoundException:  {}", ex.getMessage(), ex);
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RefreshTokenDoesNotMatchException.class)
     public ResponseEntity<ErrorResponse> hdRefreshTokenMismatch(RefreshTokenDoesNotMatchException ex) {
+        log.error("RefreshTokenDoesNotMatchException:  {}", ex.getMessage(), ex);
         HttpStatus httpStatus = ex.getHttpStatus();
         ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse,httpStatus);
     }
     @ExceptionHandler(TokenExpiredException.class)
     public ResponseEntity<ErrorResponse> hdTokenExpired(TokenExpiredException ex) {
+        log.error("TokenExpiredException:  {}", ex.getMessage(), ex);
         HttpStatus httpStatus = ex.getHttpStatus();
         ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse,httpStatus);
@@ -49,6 +57,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TokenTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> hdTypeMismatch(TokenTypeMismatchException ex) {
+        log.error("TokenTypeMismatchException:  {}", ex.getMessage(), ex);
         HttpStatus httpStatus = ex.getHttpStatus();
         ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse,httpStatus);
@@ -56,6 +65,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UnauthenticatedException.class)
     public ResponseEntity<ErrorResponse> hdUnauthenticatedEx(UnauthenticatedException ex) {
+        log.error("UnauthenticatedException:  {}", ex.getMessage(), ex);
         HttpStatus httpStatus = ex.getHttpStatus();
         ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), ex.getMessage());
         return new ResponseEntity<>(errorResponse,httpStatus);
